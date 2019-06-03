de976da812f302e13ae659fe1521503831203af0
Antonenko Bohdan <bohdan1997@gmail.com>
2019-06-03 17:16:14 +0300

    'solution'

---
+++ js.js
@@ -0,0 +1,51 @@
+'use strict';
+
+class Numberdrome {
+    constructor() {
+        this.list = [];
+    }
+
+    addNumber(n) {
+        this.list.push(n);
+        return this.list;
+    }
+
+    removeNumber(n) {
+        this.list.splice(this.list.indexOf(n), 1)
+        return this.list;
+    }
+
+    sum() {
+        let sumTotal = 0;
+        if (this.list.length !== 0) {
+        for (let i=0; i<= this.list.length; i++) {
+            sumTotal += this.list.length[i];
+            return sumTotal;
+        }};
+        return 0;
+    }
+
+    product() {
+        let prodTotal = 0;
+        if (this.list.length !== 0) {
+            for (let i=0; i<= this.list.length; i++) {
+                prodTotal *= this.list[i];
+                return prodTotal;
+            }
+        } return 1;
+    }
+
+    min() {
+        let minNum;
+        if (this.list.length !== 0) {
+            return minNum = Math.min(...this.list);
+        } else throw new Error('Mistake');
+    }
+
+    max() {
+        let maxNum;
+        if (this.list.length !== 0) {
+            return maxNum = Math.max(...this.list);
+        } else throw new Error('Mistake');
+    }
+}
