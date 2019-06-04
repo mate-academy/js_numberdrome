8f85386864aef342d0bc3d0dbace6157c9c9c92c
Antonenko Bohdan <bohdan1997@gmail.com>
2019-06-04 15:51:25 +0300

    'new_changes'

--- js.js
+++ js.js
@@ -1,51 +1,52 @@
 'use strict';
 
 class Numberdrome {
-    constructor() {
-        this.list = [];
-    }
+  constructor() {
+      this.list = [];
+  }
 
-    addNumber(n) {
-        this.list.push(n);
-        return this.list;
-    }
+  addNumber(n) {
+      this.list.push(n);
+  }
 
-    removeNumber(n) {
-        this.list.splice(this.list.indexOf(n), 1)
-        return this.list;
-    }
+   removeNumber(n) {
+      this.list.splice(this.list.indexOf(n), 1)
+  };
 
-    sum() {
-        let sumTotal = 0;
-        if (this.list.length !== 0) {
-        for (let i=0; i<= this.list.length; i++) {
-            sumTotal += this.list.length[i];
-            return sumTotal;
-        }};
-        return 0;
-    }
+  sum() {
+      let sumTotal = 0;
+      if (this.list.length !== 0) {
+      for (let i = 0; i <= this.list.length; i++) {
+          sumTotal += this.list.length[i];
+          return sumTotal;
+      }};
 
-    product() {
-        let prodTotal = 0;
-        if (this.list.length !== 0) {
-            for (let i=0; i<= this.list.length; i++) {
-                prodTotal *= this.list[i];
-                return prodTotal;
-            }
-        } return 1;
-    }
+      return sumTotal = 0;
+  }
 
-    min() {
-        let minNum;
-        if (this.list.length !== 0) {
-            return minNum = Math.min(...this.list);
-        } else throw new Error('Mistake');
-    }
+  product() {
+      let prodTotal = 0;
+      if (this.list.length !== 0) {
+          for (let i=0; i<= this.list.length; i++) {
+              prodTotal *= this.list[i];
+              return prodTotal;
+          }
+      }
 
-    max() {
-        let maxNum;
-        if (this.list.length !== 0) {
-            return maxNum = Math.max(...this.list);
-        } else throw new Error('Mistake');
-    }
+      return 1;
+  }
+
+  min() {
+      let minNum;
+      if (this.list.length !== 0) {
+          return minNum = Math.min(...this.list);
+      } else throw new Error('Mistake');
+  }
+
+  max() {
+      let maxNum;
+      if (this.list.length !== 0) {
+          return maxNum = Math.max(...this.list);
+      } else throw new Error('Mistake');
+  }
 }
