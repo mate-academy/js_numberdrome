1d2d16975da7108f9e33ddd08ed0e011ecd88478
Olegus <gulldan1996@gmail.com>
2019-06-04 22:46:50 +0300

    numberdrome

---
+++ js/script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+class Numberdrome {
+  constructor() {
+    this.numberList = [];
+  }
+
+  addNumber(n) {
+    this.numberList.push(n)
+  }
+
+  removeNumber(n) {
+    this.numberList.splice(this.numbers.indexOf(n), 1);
+  }
+
+  sum() {
+    return this.numberList.reduce((acc, num) => acc += num, 0);
+  }
+
+  product() {
+    return this.numberList.reduce((acc, num) => acc *= num, 1);
+  }
+
+  min() {
+    try {
+      if (this.numberList.length) {
+        return Math.min(...this.numberList);
+      }
+      throw new Error('there are no numbers');
+    } catch (error) {
+      return error.message;
+      }
+  }
+  max() {
+    try {
+      if (this.numberList.length) {
+        return Math.max(...this.numberList);
+      }
+      throw new Error('there are no numbers');
+    } catch (error) {
+      return error.message;
+      }
+  }
+}
+
+let test = new Numberdrome();
+test.addNumber(7);
+test.addNumber(13);
+test.addNumber(17);
+test.addNumber(24);
+test.addNumber(32);
+test.addNumber(54);
+console.log('sum =', test.sum());
+console.log('product =', test.product());
+console.log('min number =', test.min());
+console.log('max number =', test.max());
