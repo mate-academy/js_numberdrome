7999fa8038f0860a05b3d41a0164feed0782a723
AnyaLisna <anyalesnaya2812@gmail.com>
2019-06-04 18:08:30 +0300

    spaces

--- src/script.js
+++ src/script.js
@@ -2,38 +2,38 @@
 
 class Numberdrome {
   constructor() {
-		this.numbers =[];
-	} 
+    this.numbers =[];
+  } 
 
-	addNumber(n) {
-		this.numbers.push(n);
-	}
+  addNumber(n) {
+    this.numbers.push(n);
+  }
 
-	removeNumber(n) {
-		const index = this.numbers.indexOf(n);
-		this.numbers.splice(index, 1);
-	}
+  removeNumber(n) {
+    const index = this.numbers.indexOf(n);
+    this.numbers.splice(index, 1);
+  }
 
-	sum() {
-		return this.numbers.reduce((sum, number) => sum + number, 0);
-	}
+  sum() {
+    return this.numbers.reduce((sum, number) => sum + number, 0);
+  }
 
-	product() {
-		return this.numbers.reduce((sum, number) => sum * number, 1);
-	}
+  product() {
+    return this.numbers.reduce((sum, number) => sum * number, 1);
+  }
 
-	min() {
-		if(this.numbers.length) {
-			return Math.min(...this.numbers);
-		}
-		throw new Error('There are no numbers');	
-	}
+  min() {
+    if(this.numbers.length) {
+      return Math.min(...this.numbers);
+    }
+    throw new Error('There are no numbers');	
+  }
 
-	max() {
-		if(this.numbers.length) {
-			return Math.max(...this.numbers);
-		}
-		throw new Error('There are no numbers');
-	}
+  max() {
+    if(this.numbers.length) {
+      return Math.max(...this.numbers);
+    }
+    throw new Error('There are no numbers');
+  }
 }
 
