0f8d2fd236f52871fb79b2df7506d54c0e84c090
AnyaLisna <anyalesnaya2812@gmail.com>
2019-06-04 17:08:24 +0300

    fixed after TA comments

--- src/script.js
+++ src/script.js
@@ -1,34 +1,39 @@
 'use strict';
 
 class Numberdrome {
+  constructor() {
+		this.numbers =[];
+	} 
 
-    constructor() {
-        this.numbers =[];
-    } 
+	addNumber(n) {
+		this.numbers.push(n);
+	}
 
-    addNumber(n) {
-        this.numbers.push(n);
-    }
-    
-    removeNumber(n) {
-        const index = this.numbers.indexOf(n);
-        this.numbers.splice(index, 1);
-    }
+	removeNumber(n) {
+		const index = this.numbers.indexOf(n);
+		this.numbers.splice(index, 1);
+	}
 
-    sum() {
-       return this.numbers.reduce((sum, number) => sum + number, 0);
-    }
+	sum() {
+		return this.numbers.reduce((sum, number) => sum + number, 0);
+	}
 
-    product() {
-        return this.numbers.reduce((sum, number) => sum * number, 1);
-    }
+	product() {
+		return this.numbers.reduce((sum, number) => sum * number, 1);
+	}
 
-    min() {
-        return Math.min(...this.numbers);
-    }
+	min() {
+		if(this.numbers.length) {
+			return Math.min(...this.numbers);
+		}
+		throw new Error('There are no numbers');	
+	}
 
-    max() {
-        return Math.max(...this.numbers);
-    }
+	max() {
+		if(this.numbers.length) {
+			return Math.max(...this.numbers);
+		}
+		throw new Error('There are no numbers');
+	}
 }
 
