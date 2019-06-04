eb320c8fab19264d7d8e5cfc268da51e34142755
Stas Tselovanskyi <solmirx1@gmail.com>
2019-06-04 19:17:08 +0300

    fixedSomeErrors

--- main.js
+++ main.js
@@ -8,14 +8,15 @@ class Numberdrome {
   addNumber(n) {
   	if (typeof n === 'number') {
   	  this.numbers.push(n);
+    }
   }
- }
 
   removeNumber(n) {
-  	if (this.numbers.indexof(n) === -1) { return
+  	if (this.numbers.indexOf(n) === -1) { return
   	};
-  	this.numbers.splice(this.n.indexof(n), 1);
+  	this.numbers.splice(this.numbers.indexOf(n), 1);
   }
+
   sum() {
     return this.numbers.reduce(((a, b) => a + b), 0);
   }
@@ -31,5 +32,17 @@ class Numberdrome {
   max() {
     return Math.max(...this.numbers)
   }
-};
+}
+
+const numberdrome = new Numberdrome();
+numberdrome.addNumber(2);
+numberdrome.addNumber(0.5);
+numberdrome.addNumber(1);
+numberdrome.addNumber(3);
+numberdrome.removeNumber(5);
+console.log(numberdrome.min());
+console.log(numberdrome);
+numberdrome.removeNumber(0.5);
+console.log(numberdrome);
+console.log(numberdrome.min());
 
