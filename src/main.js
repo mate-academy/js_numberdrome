ce91a52c37464588c4c8751a0d4aa2006a260653
annakondr <tyukondra@gmail.com>
2019-06-05 21:30:14 +0300

    solution_with_requested_changes

--- src/main.js
+++ src/main.js
@@ -1,56 +1,51 @@
 'use strict';
 
 class Numberdrome {
-    constructor() {
-        this.numbers = [];
-    }
+  constructor() {
+    this.numbers = [];
+  }
 
-    addNumber(n) {
-        this.numbers.push(n);
-        return this.numbers
+  addNumber(n) {
+    if (typeof n === 'number'){
+      this.numbers.push(n);
     }
+  }
 
-    removeNumber(n) {
-        return this.numbers.filter(num => num !== n)
-    }
+  removeNumber(n) {
+    return this.numbers.filter(num => num !== n)
+  }
 
-    sum() {
-        if (this.numbers.length !== 0) {
-            return this.numbers.reduce((a, b) => {return a + b})
-        }
-        return 0;
-    }
+  sum() {
+    return this.numbers.reduce(((a, b) => a + b), 0)
+  }
 
-    product() {
-        if (this.numbers.length !== 0) {
-            return this.numbers.reduce((a, b) => {return a * b})
-        }
-        return 1;
-    }
+  product() {
+    return this.numbers.reduce(((a, b) => a * b), 1)
+  }
 
-    min() {
-        if (this.numbers.length !== 0) {
-            return Math.min.apply(null, this.numbers)
-        }
-        return ('Error: The Numberdrome is empty')
+  min() {
+    if (this.numbers.length === 0) {
+      throw 'Error: The Numberdrome is empty'
     }
+    return Math.min(...this.numbers)
+  }
 
-    max() {
-        if (this.numbers.length !== 0) {
-            return Math.max.apply(null, this.numbers)
-        }
-        return ('Error: The Numberdrome is empty')
+  max() {
+    if (this.numbers.length === 0) {
+      throw 'Error: The Numberdrome is empty'
     }
+    return Math.max(...this.numbers)
+  }
 
 }
 
 const numbersList = new Numberdrome();
-console.log(numbersList.addNumber(56));
-console.log(numbersList.addNumber(102));
-console.log(numbersList.addNumber(3));
-console.log(numbersList.addNumber(507));
-console.log(numbersList.addNumber(81));
-console.log(numbersList.addNumber(102));
+numbersList.addNumber(56);
+numbersList.addNumber(102);
+numbersList.addNumber(3);
+numbersList.addNumber(507);
+numbersList.addNumber(81);
+numbersList.addNumber(102);
 
 console.log(numbersList.removeNumber(102));
 
@@ -58,5 +53,3 @@ console.log(numbersList.sum());
 console.log(numbersList.product());
 console.log(numbersList.min());
 console.log(numbersList.max());
-
-
