e0db8ae2d59cfd613805bcc744559dc47c9991d3
Batsenko <48134243+Batsenko@users.noreply.github.com>
2019-06-07 11:01:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
+'use strict';
+
 class Numberdrome {
-    constructor() {
-        this.intArray = [];
-    }
+  constructor() {
+      this.intArray = [];
+  }
+  addNumber(n) {
+      this.intArray.push(n);
+  }
+  removeNumber(n) {
+      let index = this.intArray.indexOf(n);
+      if (index > -1) {
+          this.intArray.splice(index, 1);
+      }
+  }
 
-    addNumber(n) {
-        this.intArray.push(n);
-    }
+  sum() {
+      return this.intArray.reduce((accumulator, a) => accumulator + a, 0);
+  }
 
-    removeNumber(n) {
-        let index = this.intArray.indexOf(n);
-        if (index > -1) {
-            this.intArray.splice(index, 1);
-        }
-    }
+  product() {
+      return this.intArray.reduce((accumulator, a) => accumulator * a, 1);
+  }
 
-    sum() {
-        return this.intArray.reduce((accumulator, a) => accumulator + a, 0);
-    }
-
-    product() {
-        return this.intArray.reduce((accumulator, a) => accumulator * a, 1);
-    }
-
-    min() {
-        if(this.intArray.length === 0) throw "exception";
+  min() {
+      if(this.intArray.length === 0) throw "exception";
         
-        return Math.min.apply(null, this.intArray);
-    }
+      return Math.min.apply(null, this.intArray);
+  }
 
-    max() {
-        if(this.intArray.length === 0) throw "exception";
-    
-        return Math.max.apply(null, this.intArray);
-    }
+  max() {
+      if(this.intArray.length === 0) throw "exception";
+  
+      return Math.max.apply(null, this.intArray);
+  }
 }
