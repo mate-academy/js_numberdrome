bd285e3c9fc3341d07999345bae94ebe1b2c0e89
StasGostev <spaceomen@gmail.com>
2019-06-07 15:20:57 +0300

    'Fixed_remarks'

--- main.js
+++ main.js
@@ -1,40 +1,38 @@
 "use strict";
 
 class Numberdrome {
-    constructor() {
-        this.numberList = []; 
-        this.errorIsEmpty = 'Number list is Empty';
-    }
+  constructor() {
+    this.numberList = [];
+    this.errorIsEmpty = "Number list is Empty";
+  }
 
-    addNumber(n) {
-        this.numberList.push(n);
-    }
+  addNumber(n) {
+    this.numberList.push(n);
+  }
 
-    removeNumber(n) {
-        this.numberList.splice(this.numberList.indexOf(n), 1)
-    }
+  removeNumber(n) {
+    this.numberList.splice(this.numberList.indexOf(n), 1);
+  }
 
-    sum() {
-        return this.numberList.reduce((acc, n) => acc + n, 0);
-    }
+  sum() {
+    return this.numberList.reduce((acc, n) => acc + n, 0);
+  }
 
-    product() {
-        return this.numberList.reduce((acc, n) => acc * n, 1);
-    }
+  product() {
+    return this.numberList.reduce((acc, n) => acc * n, 1);
+  }
 
-    min() {
-        if (this.numberList.length !== 0) {
-            return Math.min(...this.numberList);
-        } else {
-            throw new Error(this.errorIsEmpty);
-        }
+  min() {
+    if (this.numberList.length !== 0) {
+      return Math.min(...this.numberList);
     }
+    throw new Error(this.errorIsEmpty);
+  }
 
-    max() {
-        if (this.numberList.length !== 0) {
-            return Math.max(...this.numberList);
-        } else {
-            throw new Error(this.errorIsEmpty)
-        }
+  max() {
+    if (this.numberList.length !== 0) {
+      return Math.max(...this.numberList);
     }
+    throw new Error(this.errorIsEmpty);
+  }
 }
