6a26134628844b6efc47c3d7df0d24c5022bfbaa
Andrii <gaarasandy@gmail.com>
2019-06-08 15:33:32 +0300

    add Throw error

--- main.js
+++ main.js
@@ -1,32 +1,38 @@
 'use strict';
 
 class Numberdrome {
-  constructor(){
+  constructor() {
     this.num = [];
   }
 
   addNumber(n) {
-    if (typeof n === 'number'){
+    if (typeof n === 'number') {
       this.num.push(n);
     }
   }
   removeNumber(n) {
     let index = this.num.indexOf(n);
-    if(index === -1){
+    if (index === -1) {
       return;
     }
     this.num.splice(index, 1);
   }
-  sum(){
+  sum() {
     return this.num.reduce(((a, b) => a + b), 0);
   }
   product() {
     return this.num.reduce(((a, b) => a * b), 1);
   }
-  min(){
-    return Math.min(...this.num);
+  min() {
+    if (this.num.length) {
+      return Math.min(...this.num);
+    }
+    throw new Error('There are no numbers');
   }
-  max(){
-    return Math.max(...this.num);
+  max() {
+    if (this.num.length) {
+      return Math.max(...this.num);
+    }
+    throw new Error('There are no numbers');
   }
 }
