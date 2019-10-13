2cb7996236c69ade8ff8d0d7f10b1ac3bbeaa846
dsrtF0x-git <54433971+dsrtF0x-git@users.noreply.github.com>
2019-10-13 22:58:32 +0300

    Update numberdrome.js

--- numberdrome.js
+++ numberdrome.js
@@ -8,7 +8,11 @@ class Numberdrome {
   }
 
   removeNumber(n) {
-    this.list.splice(this.list.indexOf(n), 1);
+    if (this.list.includes(n)) {
+      this.list.splice(this.list.indexOf(n), 1);
+    } else {
+      return `There is no such number`;
+    }
   }
 
   sum() {
@@ -20,10 +24,10 @@ class Numberdrome {
   }
 
   min() {
-    return this.list.length === 0 ? `Your list is empty` : Math.min(...this.list);
+    return this.list.length ? Math.min(...this.list) : `Your list is empty`;
   }
 
   max() {
-    return this.list.length === 0 ? `Your list is empty` : Math.max(...this.list);
+    return this.list.length ? Math.max(...this.list) : `Your list is empty`;
   }
 }
