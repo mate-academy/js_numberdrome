06e28fe7985becf6ab9a95b13a42168344ff704e
VadimK <djvadya16@gmail.com>
2019-10-14 13:19:56 +0300

    done

--- src/numberDrome.js
+++ src/numberDrome.js
@@ -7,9 +7,9 @@ class Numberdrome {
     for (let i = 0; i < n.length; i++) {
       if (isFinite(n[i])) {
         this.numbersArr.push(n[i]); 
-      } else (
-        console.log(`${n[i]} is not a number. Try only number`)
-      )
+      } else {
+        return `${n[i]} is not a number. Try only number`; 
+      }  
     }
   }
 
@@ -18,23 +18,23 @@ class Numberdrome {
     if (index != -1) {
       this.numbersArr.splice(index, 1); 
     } else {
-      console.log('There is nothing to delete'); 
+      return 'There is nothing to delete'; 
     }
   }
 
   sum() {
-    console.log(this.numbersArr.reduce((sum, current) => sum + current)); 
+    return this.numbersArr.reduce((sum, current) => sum + current); 
   }
 
   product() {
-    console.log(this.numbersArr.reduce((sum, current) => sum * current));
+    return this.numbersArr.reduce((sum, current) => sum * current);
   }
 
   min() {
-    console.log(Math.min.apply(null, this.numbersArr)); 
+    return Math.min.apply(null, this.numbersArr); 
   }
 
   max() {
-    console.log(Math.max.apply(null, this.numbersArr)); 
+    return Math.max.apply(null, this.numbersArr); 
   }
 }
