9a1b760cf53f0ced15c22edd992a8d9068f317ad
Liliia <chuiko1112@gmail.com>
2020-06-09 14:20:51 +0300

    solution

--- src/Numberdrome.js
+++ src/Numberdrome.js
@@ -1,7 +1,33 @@
 'use strict';
 
 class Numberdrome {
-  // write code here
+  constructor() {
+    this.list = [];
+  };
+  addNumber(n) {
+    this.list.push(n);
+  }
+  removeNumber(n) {
+    this.list = this.list.filter(element => element !== n);
+
+    return this.list;
+  }
+
+  sum() {
+    return this.list.reduce((sum, current) => sum + current, 0);
+  }
+
+  product() {
+    return this.list.reduce((sum, current) => sum * current, 1);
+  }
+
+  min() {
+    return (this.list.length === 0) ? Error : Math.min.apply(null, this.list);
+  }
+
+  max() {
+    return (this.list.length === 0) ? Error : Math.max.apply(null, this.list);
+  }
 }
 
 module.exports = {
