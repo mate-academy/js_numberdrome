0410e6f7f2a304b5bca51810e132b6d9492d253b
denkondratiev <denys.kondratiev.job@gmail.com>
2020-06-10 21:22:32 +0300

    first

--- src/Numberdrome.js
+++ src/Numberdrome.js
@@ -1,7 +1,45 @@
 'use strict';
 
 class Numberdrome {
-  // write code here
+  constructor() {
+    this.numbers = [];
+  };
+
+  addNumber(n) {
+    this.numbers.push(n);
+  }
+
+  removeNumber(n) {
+    this.numbers = this.numbers.filter(number => number !== n);
+  }
+
+  sum() {
+    return this.numbers.reduce((prev, cur) => prev + cur, 0);
+  }
+
+  product() {
+    return this.numbers.reduce((prev, cur) => prev * cur, 1);
+  }
+
+  min() {
+    let result;
+
+    this.numbers.length === 0
+      ? result = 'List empty'
+      : result = Math.min(...this.numbers);
+
+    return result;
+  }
+
+  max() {
+    let result;
+
+    this.numbers.length === 0
+      ? result = 'List empty'
+      : result = Math.max(...this.numbers);
+
+    return result;
+  }
 }
 
 module.exports = {
