703e2758272a60151088711349ff95f315d7b63d
Igor Kulish <igormagnetic@gmail.com>
2020-06-10 22:14:02 +0300

    solution

--- src/Numberdrome.js
+++ src/Numberdrome.js
@@ -1,7 +1,37 @@
 'use strict';
 
 class Numberdrome {
-  // write code here
+  constructor(...args) {
+    this.args = args;
+  }
+
+  addNumber(n) {
+    this.args.push(n);
+  }
+
+  removeNumber(n) {
+    this.args = this.args.filter(element => element !== n);
+  }
+
+  sum() {
+    return this.args.reduce((a, b) => a + b);
+  }
+
+  product() {
+    if (this.args.length === 0) {
+      return 1;
+    }
+
+    return this.args.reduce((a, b) => a * b);
+  }
+
+  min() {
+    return Math.min(...this.args);
+  }
+
+  max() {
+    return Math.max(...this.args);
+  }
 }
 
 module.exports = {
