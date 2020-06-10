8a6043b63df6b7e078ed8d17deb26425e6a356bb
solaryasha <solaryasha@gmail.com>
2020-06-10 19:58:12 +0300

    My solution

--- src/Numberdrome.js
+++ src/Numberdrome.js
@@ -1,7 +1,42 @@
 'use strict';
 
 class Numberdrome {
-  // write code here
+  constructor() {
+    this.list = [];
+  }
+
+  addNumber(n) {
+    this.list.push(n);
+  }
+
+  removeNumber(n) {
+    this.list = this.list.filter(number => number !== n);
+  }
+
+  sum() {
+    return this.list.length
+      ? this.list.reduce((accum, current) => accum + current, 0)
+      : 0;
+  }
+
+  product() {
+    return this.list.length
+      ? this.list.reduce((accum, current) => accum * current)
+      : 1
+      ;
+  }
+
+  min() {
+    return this.list.length
+      ? Math.min(...this.list)
+      : new Error('There are no numbers');
+  }
+
+  max() {
+    return this.list.length
+      ? Math.max(...this.list)
+      : new Error('There are no numbers');
+  }
 }
 
 module.exports = {
