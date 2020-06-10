fb9c5341350ae69252cceda891ac62b5b819d1fd
LimonovInsp <bis.limonov@gmail.com>
2020-06-10 16:38:56 +0300

    solution

--- src/Numberdrome.js
+++ src/Numberdrome.js
@@ -1,7 +1,39 @@
 'use strict';
 
 class Numberdrome {
-  // write code here
+  constructor() {
+    this.listOfNumber = [];
+  }
+
+  addNumber(n) {
+    this.listOfNumber.push(n);
+  }
+
+  removeNumber(n) {
+    while (this.listOfNumber.indexOf(n) !== -1) {
+      this.listOfNumber.splice(this.listOfNumber.indexOf(n), 1);
+    }
+  }
+
+  sum() {
+    return this.listOfNumber.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue;
+    });
+  }
+
+  product() {
+    return this.listOfNumber.reduce((accumulator, currentValue) => {
+      return accumulator * currentValue;
+    });
+  }
+
+  min() {
+    return Math.min.apply(null, this.listOfNumber);
+  }
+
+  max() {
+    return Math.max.apply(null, this.listOfNumber);
+  }
 }
 
 module.exports = {
