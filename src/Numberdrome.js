0bd398bd34f79006992066a66e4830ab3b3c0f27
Campari229 <KennyKoin@gmail.com>
2020-06-11 16:04:45 +0300

    Solution

--- src/Numberdrome.js
+++ src/Numberdrome.js
@@ -1,7 +1,33 @@
 'use strict';
 
 class Numberdrome {
-  // write code here
+  constructor() {
+    this.numbers = [];
+  }
+
+  addNumber(n) {
+    this.numbers.push(n);
+  }
+
+  removeNumber(n) {
+    this.numbers = this.numbers.filter(number => number !== n);
+  }
+
+  sum() {
+    return this.numbers.reduce((number, acumulator) => acumulator + number, 0);
+  }
+
+  product() {
+    return this.numbers.reduce((number, acumulator) => acumulator * number, 1);
+  }
+
+  min() {
+    return Math.min(...this.numbers);
+  }
+
+  max() {
+    return Math.max(...this.numbers);
+  }
 }
 
 module.exports = {
