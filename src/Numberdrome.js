96c8ab01706ce657626d3ddb2a6fd5e1b703cdd5
YanG-6668 <goch.yannn@gmail.com>
2020-06-11 09:53:31 +0300

    class-2

--- src/Numberdrome.js
+++ src/Numberdrome.js
@@ -1,7 +1,39 @@
 'use strict';
 
 class Numberdrome {
-  // write code here
+  constructor() {
+    this.arrayOfNumbers = [];
+  }
+
+  addNumber(num) {
+    this.arrayOfNumbers.push(num);
+  }
+
+  removeNumber(num) {
+    this.arrayOfNumbers = this.arrayOfNumbers.filter(a => a !== num);
+  }
+
+  sum() {
+    return this.arrayOfNumbers.reduce((a, b) => {
+      return a + b;
+    });
+  }
+
+  product() {
+    return this.arrayOfNumbers.length === 0
+      ? 1
+      : this.arrayOfNumbers.reduce((a, b) => {
+        return a * b;
+      });
+  }
+
+  min() {
+    return Math.min(...this.arrayOfNumbers);
+  }
+
+  max() {
+    return Math.max(...this.arrayOfNumbers);
+  }
 }
 
 module.exports = {
