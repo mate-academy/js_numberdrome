d63ff8f2ebe600fb79a436d77cfa4274dbb73e37
tyooma <flying.alien.godzilla@gmail.com>
2020-06-11 15:36:41 +0300

    NUMBERDROME solution

--- src/Numberdrome.js
+++ src/Numberdrome.js
@@ -1,7 +1,39 @@
 'use strict';
 
 class Numberdrome {
-  // write code here
+  constructor() {
+    this.list = [];
+  }
+
+  addNumber(n) {
+    this.list.push(n);
+  }
+
+  removeNumber(n) {
+    this.list = this.list.filter(number => number !== n);
+  }
+
+  sum() {
+    return this.list.reduce(
+      (accumulator, currentNumber) => accumulator + currentNumber, 0);
+  }
+
+  product() {
+    return this.list.reduce(
+      (accumulator, currentNumber) => accumulator * currentNumber, 1);
+  }
+
+  min() {
+    return this.list.length > 0
+      ? Math.min(...this.list)
+      : Error;
+  }
+
+  max() {
+    return this.list.length > 0
+      ? Math.max(...this.list)
+      : Error;
+  }
 }
 
 module.exports = {
