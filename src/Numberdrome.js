1d3e1134de5f463d91f51fb4b9769b47db8e3a6b
Andrew Nickyforov <nik_andrey@ukr.net>
2020-06-14 17:12:01 +0300

    solution

--- src/Numberdrome.js
+++ src/Numberdrome.js
@@ -1,7 +1,37 @@
 'use strict';
 
 class Numberdrome {
-  // write code here
+  constructor() {
+    this.numbers = [];
+  }
+
+  addNumber(n) {
+    this.numbers.push(n);
+  }
+
+  removeNumber(n) {
+    this.numbers = this.numbers.filter(number => number !== n);
+  }
+
+  sum() {
+    return this.numbers.reduce((sum, number) => sum + number, 0);
+  }
+
+  product() {
+    return this.numbers.reduce((product, number) => product * number, 1);
+  }
+
+  min() {
+    return this.numbers.length
+      ? Math.min(...this.numbers)
+      : 'error';
+  }
+
+  max() {
+    return this.numbers.length
+      ? Math.max(...this.numbers)
+      : 'error';
+  }
 }
 
 module.exports = {
